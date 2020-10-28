22f8a193c72925426bed1f6fce86c6f0f6b11c6f
Fabien Wirig <fabien.wirig@gmail.com>
2020-10-28 23:09:53 +0100

    bed size + lin advance

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1092,12 +1092,12 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 215
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 225 //235 to full
+#define Y_BED_SIZE 209 //235 to full
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS -10
+#define X_MIN_POS -14 //-9 to full
+#define Y_MIN_POS -19 //-6 to full
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
